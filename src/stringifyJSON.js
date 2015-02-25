8cebc654fbd333bee6fcbb35170573ae2902416b
Yuriy Bash <yuriybash@gmail.com>
2015-02-25 14:37:15 -0800

    finished stringifyJSON through arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,22 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+    // your code goes here
 
-  console.log("obj: " + obj);
-  console.log("typeof obj: " + typeof obj);
+    // console.log("obj: " + obj);
+    // console.log("typeof obj: " + typeof obj);
 
-  if(typeof obj === 'number'){
-  	return "" + obj;
-  }
-  else if (obj === null){
-  	return "" + obj;
-  }
-  else if (typeof obj == 'boolean'){
-  	return "" + obj
-  }
+    if (typeof obj === 'number') {
+        return "" + obj;
+    } else if (obj === null) {
+        return "" + obj;
+    } else if (typeof obj == 'boolean') {
+        return "" + obj
+    } else if (typeof obj == 'string') {
+        return "\"" + obj + "\"";
+    } else if (Array.isArray(obj)) {
+        var result ="";
 
-  return "" + obj;
+        if (obj.length == 0) {
+            return '[]';
+        } else {
+        	
+        	for (var i = 0; i <= obj.length-1; i++) {
+        		
+        		result += stringifyJSON(obj[i]);
+        		if(i >= 0 && i < obj.length-1){
+        			console.log("TEST")
+        			result += ","
+        		}
+        	};
+
+        	console.log(result)
+        	return ("[" + result + "]")
+
+
+        }
+    }
+
 
 
 
